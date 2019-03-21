225e5378c3ba9af9e60aab86018f563961338bf3
WUST-mengqinyu <2017051104011@std.uestc.edu.cn>
2019-03-21 15:13:06 +0800

    test

--- src/pages/oj/views/contest/ContestDetail.vue
+++ src/pages/oj/views/contest/ContestDetail.vue
@@ -33,44 +33,44 @@
 
     </div>
     <div v-show="showMenu" id="contest-menu">
-      <VerticalMenu @on-click="handleRoute">
-        <VerticalMenu-item :route="{name: 'contest-details', params: {contestID: contestID}}">
+      <Menu @on-click="handleRoute">
+        <item :route="{name: 'contest-details', params: {contestID: contestID}}">
           <Icon type="home"></Icon>
           {{$t('m.Overview')}}
-        </VerticalMenu-item>
+        </item>
 
-        <VerticalMenu-item :disabled="contestMenuDisabled"
+        <item :disabled="contestMenuDisabled"
                            :route="{name: 'contest-announcement-list', params: {contestID: contestID}}">
           <Icon type="chatbubble-working"></Icon>
           {{$t('m.Announcements')}}
-        </VerticalMenu-item>
+        </item>
 
-        <VerticalMenu-item :disabled="contestMenuDisabled"
+        <item :disabled="contestMenuDisabled"
                            :route="{name: 'contest-problem-list', params: {contestID: contestID}}">
           <Icon type="ios-photos"></Icon>
           {{$t('m.Problems')}}
-        </VerticalMenu-item>
+        </item>
 
-        <VerticalMenu-item v-if="OIContestRealTimePermission"
+        <item v-if="OIContestRealTimePermission"
                            :disabled="contestMenuDisabled"
                            :route="{name: 'contest-submission-list'}">
           <Icon type="navicon-round"></Icon>
           {{$t('m.Submissions')}}
-        </VerticalMenu-item>
+        </item>
 
-        <VerticalMenu-item v-if="OIContestRealTimePermission"
+        <item v-if="OIContestRealTimePermission"
                            :disabled="contestMenuDisabled"
                            :route="{name: 'contest-rank', params: {contestID: contestID}}">
           <Icon type="stats-bars"></Icon>
           {{$t('m.Rankings')}}
-        </VerticalMenu-item>
+        </item>
 
-        <VerticalMenu-item v-if="showAdminHelper"
+        <item v-if="showAdminHelper"
                            :route="{name: 'acm-helper', params: {contestID: contestID}}">
           <Icon type="ios-paw"></Icon>
           {{$t('m.Admin_Helper')}}
-        </VerticalMenu-item>
-      </VerticalMenu>
+        </item>
+      </Menu>
     </div>
   </div>
 </template>
