bdb906e15854c969ab60cf6608cc5eff3a10500f
WUST-mengqinyu <2017051104011@std.uestc.edu.cn>
2019-06-29 23:09:48 +0800

    unfinished rejudge 界面.

---
+++ src/pages/admin/views/general/Rejudge.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="flex-container">
+        <ul class="filter">
+            <li>
+                <Input v-model="formFilter.username" placeholder="Search Author" @on-enter="handleQueryChange"/>
+            </li>
+            <li>
+                <Input v-model="formFilter.username" placeholder="Search Author" @on-enter="handleQueryChange"/>
+            </li>
+            <li>
+                <Button type="info" icon="refresh" @click="getSubmissions">{{$t('m.Refresh')}}</Button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: 'Rejudge'
+    }
+</script>
+
+<style scoped lang="less">
+    .ivu-btn-text {
+        color: #57a3f3;
+    }
+
+    .flex-container {
+        #main {
+            flex: auto;
+            margin-right: 18px;
+            .filter {
+                margin-right: -10px;
+            }
+        }
+        #contest-menu {
+            flex: none;
+            width: 210px;
+        }
+    }
+</style>
